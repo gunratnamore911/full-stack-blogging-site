7e78e7cf1102693f5bf880f705d42514f8b45d77
gunratnamore911 <gunratnamore@gmail.com>
2021-04-13 14:09:04 +0530

    register

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,8 +1,24 @@
 import React from "react";
 import Navbar from "./Navbar";
-
+import { Paper, TextField } from "@material-ui/core";
 const Login = () => {
-  return <Navbar />;
+  return (
+    <>
+      <div className="re1">
+        <Navbar />
+
+        <div className="register">
+          <h1 className="heading">Login</h1> <br /> <br />
+          <br />
+          <TextField id="standard-basic" label="email" /> <br />
+          <br />
+          <TextField id="standard-basic" label="password" /> <br />
+          <br />
+          <button className="get_started">Sign In</button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Login;
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,8 +1,31 @@
 import React from "react";
+import "./Register.css";
 import Navbar from "./Navbar";
-
+import { Paper, TextField } from "@material-ui/core";
 const Register = () => {
-  return <Navbar />;
+  return (
+    <>
+      <div className="re1">
+        <Navbar />
+
+        <div className="register">
+          <h1 className="heading">Register / Sign Up</h1> <br /> <br />
+          <TextField id="standard-basic" label="name">
+            ra
+          </TextField>
+          <br />
+          <br />
+          <TextField id="standard-basic" label="email" /> <br />
+          <br />
+          <TextField id="standard-basic" label="password" /> <br />
+          <br />
+          <TextField id="standard-basic" label="confirm password" /> <br />
+          <br />
+          <button className="get_started">Sign Up</button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Register;
